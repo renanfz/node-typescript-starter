66cbd33a189d3270e509553aec6eaa3a02f09cc0
Renan <renanfsouza135@gmail.com>
2026-09-01 20:10:52 -0300

    feat: melhora a logica das rotas
    melhora a logica e tratamento de erro das rotas

---
+++ ex01.py
@@ -0,0 +1,10 @@
+
+name = str(input('Qual o seu nome: '))
+while len(name) == 0:
+     print('Ops! campo vazio. Digite novamente')
+     name = str(input('Qual o seu nome: '))
+
+
+
+print(f'Seu nome tem {len(name)} letras')
+                                                                                                    
